9909f54d6c6e9f4c12dac3dd60b2422db2b9a7f1
ldvndev <ldvndev@gmail.com>
2024-03-20 20:15:52 -0300

    feat: Added search-form

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,11 +1,13 @@
 import { PostCard } from './components/PostCard'
 import { Profile } from './components/Profile'
+import { SearchForm } from './components/SearchForm'
 import { BlogPageContainer } from './styles'
 
 export function BlogPage() {
   return (
     <>
       <Profile />
+      <SearchForm />
 
       <BlogPageContainer>
         <PostCard />
