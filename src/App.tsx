71ee36811b6dec9ce7235f067edc0b54951e3629
ldvndev <ldvndev@gmail.com>
2024-03-19 22:50:27 -0300

    feat: Add react-router-dom configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import { ThemeProvider } from "styled-components";
-import { defaultTheme } from "./styles/themes/default";
-import { GlobalStyle } from "./styles/global";
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './routes'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      
+      <RouterProvider router={router} />
+
       <GlobalStyle />
     </ThemeProvider>
   )
