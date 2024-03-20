e065bd56e19adf443bc925645b133ef7a22fac2d
ldvndev <ldvndev@gmail.com>
2024-03-20 20:37:19 -0300

    feat: Added initial React Query configuration

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,18 @@
+import { GlobalStyle } from './styles/global'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
-import { GlobalStyle } from './styles/global'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
+
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './lib/react-query'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <RouterProvider router={router} />
-
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
       <GlobalStyle />
     </ThemeProvider>
   )
